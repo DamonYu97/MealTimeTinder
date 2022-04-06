ee7e4eb37065a03845e26f95769da03027359712
ConorJEM <mcgale98@gmail.com>
2022-04-06 04:15:52 +0100

    .

---
+++ backend/src/main/java/sta/cs5031p3/mealtimetinder/backend/service/UserServiceTests.java
@@ -0,0 +1,38 @@
+package sta.cs5031p3.mealtimetinder.backend.service;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import sta.cs5031p3.mealtimetinder.backend.model.User;
+import sta.cs5031p3.mealtimetinder.backend.repository.UserRepository;
+import sta.cs5031p3.mealtimetinder.backend.service.impl.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserServiceTests {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService = new UserServiceImpl();
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void getAllUserTest() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(null, "damon", "120",
+                User.Status.REGISTERED, User.Role.ADMIN, null, null ));
+        users.add(new User(null, "johnny", "120",
+                User.Status.REGISTERED, User.Role.ADMIN, null, null ));
+        Mockito.when(userRepository.findAll()).thenReturn(users);
+        assertEquals(2, userService.getAllUsers().size());
+    }
+
+}
