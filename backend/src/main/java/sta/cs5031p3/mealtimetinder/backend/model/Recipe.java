433333a3eefb86c03942fc7a2ca04b01821650e1
damonyu <damonyu97@hotmail.com>
2022-04-06 19:57:14 +0100

    Finish get meals API: open to all user without login

--- backend/src/main/java/sta/cs5031p3/mealtimetinder/backend/model/Recipe.java
+++ backend/src/main/java/sta/cs5031p3/mealtimetinder/backend/model/Recipe.java
@@ -1,5 +1,6 @@
 package sta.cs5031p3.mealtimetinder.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,5 +25,6 @@ public class Recipe {
 
     @ManyToOne
     @JoinColumn(name = "meal_id", nullable = false)
+    @JsonIgnore
     private Meal meal;
 }
